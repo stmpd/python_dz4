349b252729374bb240343939685b748cecaf8a85
George.S <piligrimius@mail.ru>
2022-10-04 15:44:36 +0300

    Task_01 Completed

---
+++ Task_01.py
@@ -0,0 +1,25 @@
+
+#Вычислить число c заданной точностью d
+#in
+#Enter a real number: 9
+#Enter the required accuracy '0.0001': 0.000001
+#out
+#9.000000
+#in
+#Enter a real number: 8.98785
+#Enter the required accuracy '0.0001': 0.001
+#out
+#8.988
+
+
+from decimal import Decimal
+
+
+def set_acc(number, accur):
+    return num.quantize(accur)
+
+
+num = Decimal(input("Введите точное число: "))
+acc = Decimal(input("Введите необходимую тончность до '0.0001': "))
+
+print(set_acc(num, acc))
